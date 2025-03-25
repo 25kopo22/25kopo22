0bac602a4c8dabec571365f7e99682f71cfda59d
25kopo22 <bulal123456@gmail.com>
2025-03-25 15:43:33 +0900

    python_구구단.py

--- python_구구단.py
+++ python_구구단.py
@@ -12,7 +12,7 @@ i = 1
 while i < 10:
     j = 1
     while j < 10:
-        print(str(i) + 'X' + str(j) + '=' + str(i * j))
+        print(str(i) + ' X ' + str(j) + ' = ' + str(i * j))
         j += 2
     i += 2 
 
@@ -22,6 +22,6 @@ i = 2
 while i < 10:
     j = 2
     while j < 10:
-        print(str(i) + 'X' + str(j) + '=' + str(i * j))
+        print(str(i) + ' X ' + str(j) + ' = ' + str(i * j))
         j += 2
     i += 2 
