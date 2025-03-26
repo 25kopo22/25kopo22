95969d31d5f86254d3a9d339d3ca2914d020c0fa
25kopo22 <bulal123456@gmail.com>
2025-03-26 11:18:31 +0900

    python - 별.py

--- python - 별.py
+++ python - 별.py
@@ -1,7 +1,12 @@
-n = int(input())
-a = ' * '
-if n <= 0:
-  print('다시 입력해주세요.')
-else:
-  for i in range(1, n + 1):
-    print(a * i)
+while True:
+    try:
+        n = int(input("숫자를 입력하세요: "))
+        if n <= 0:
+            print("다시 입력해주세요.")
+        else:
+            a = ' * '
+            for i in range(1, n + 1):
+                print(a * i)
+            break 
+    except ValueError:
+        print("다시 입력해주세요.")
