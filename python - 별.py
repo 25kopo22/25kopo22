5382ea184e37c3cdd11866620f1f3a60c09ff2d0
25kopo22 <bulal123456@gmail.com>
2025-03-26 09:43:28 +0900

    python - 별.py

--- python - 별.py
+++ python - 별.py
@@ -1,9 +1,4 @@
-a = 5
-for i in range(1,10,+2):
-  print(str(a) + ' X ' + str(i) + ' = ' + str(i*a))
-#print('while 조건문을 for 조건문으로 바꾸어 사용할 수 있다!')
-
 n = int(input())
-a = '*'
-for i in range(1,n+1):
+a = ' * '
+for i in range(1, n+1):
   print(a * i)
