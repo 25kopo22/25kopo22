516791d08727e067d19496cec1165f5f37c31787
25kopo22 <bulal123456@gmail.com>
2025-03-26 11:26:41 +0900

    python - 자판기.py

--- python - 자판기.py
+++ python - 자판기.py
@@ -1,25 +1,26 @@
 while True:
-  print('음료목록 1.오렌지주스(100원), 2.커피(200원), 3.콜라(300원)')
+  try:
+    print('음료목록 1.오렌지주스(100원), 2.커피(200원), 3.콜라(300원)')
 
-  coin = int(input('동전을 넣으세요.'))
+    coin = int(input('동전을 넣으세요.'))
 
-  if coin <= 0:
-    print('유효하지 않은 동전입니다.')
-    continue
+    if coin <= 0:
+      print('유효하지 않은 동전입니다.')
+      continue
 
-  drink = int(input('음료를 고르세요.\n'))
+    drink = int(input('음료를 고르세요.\n'))
 
-  if drink == 1:
-  #오렌지주스 100원
-    if coin >= 100:
-        remain = coin - 100
-        print('오렌지주스가 곧 제공됩니다.')
-        print('거스름돈은 {}원입니다.'.format(remain))
-    else:
-        print('잔액이 부족합니다.')
+    if drink == 1:
+    #오렌지주스 100원
+      if coin >= 100:
+          remain = coin - 100
+          print('오렌지주스가 곧 제공됩니다.')
+          print('거스름돈은 {}원입니다.'.format(remain))
+      else:
+          print('잔액이 부족합니다.')
 
-  elif drink == 2:
-  #커피 200원
+    elif drink == 2:
+    #커피 200원
       if coin >= 200:
         remain = coin - 200
         print('커피가 곧 제공됩니다.')
@@ -27,8 +28,8 @@ while True:
       else:
         print('잔액이 부족합니다.')
 
-  elif drink == 3:
-  #콜라 300원
+    elif drink == 3:
+    #콜라 300원
       if coin >= 300:
         remain = coin - 300
         print('콜라가 곧 제공됩니다.')
@@ -36,6 +37,9 @@ while True:
       else:
         print('잔액이 부족합니다.')
 
-  else:
-  #없는 번호
-    print('없는 메뉴입니다. 다시 입력해 주세요.')
+    else:
+    #없는 번호
+     print('없는 메뉴입니다. 다시 입력해 주세요.')
+
+  except ValueError:
+        print("다시 입력해주세요.")
