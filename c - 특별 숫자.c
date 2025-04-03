77d3e15c891be880331811959b228266f5e64fe2
25kopo22 <bulal123456@gmail.com>
2025-04-03 18:58:00 +0900

    Add files via upload

---
+++ c - 특별 숫자.c
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+// 각 자리 숫자의 합 계산 함수
+int sumdigit(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int num;
+    
+    // 사용자 입력 받기
+    printf("0부터 999 사이의 숫자를 입력하세요: ");
+    if (scanf("%d", &num) != 1 || num < 0 || num > 999) {
+        printf("올바른 숫자를 입력하세요.\n");
+        return 1;
+    }
+    
+    // 첫 번째 자리와 마지막 자리 숫자 구하기
+    int first = num;
+    while (first >= 10) {
+        first /= 10;
+    }
+    int last = num % 10;
+    
+    // 조건 확인
+    if ((num % 3 == 0 && num % 2 == 1) ||  // 3의 배수이면서 홀수
+        (sumdigit(num) % 7 == 0) ||    // 각 자리 숫자의 합이 7의 배수
+        (first > last)) {       // 첫 번째 자리 숫자가 마지막 자리 숫자보다 큼
+        printf("%d는 특별한 숫자입니다.\n", num);
+    } else {
+        printf("%d는 특별한 숫자가 아닙니다.\n", num);
+    }
+    
+    return 0;
+}
