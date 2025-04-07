baf20e7edebcac99b61efcef8e7f7516dcf6dca2
25kopo22 <bulal123456@gmail.com>
2025-04-07 15:52:11 +0900

    c - 년월 이중포문.c

--- c - 년월 이중포문.c
+++ c - 년월 이중포문.c
@@ -1,16 +1,10 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main() {
     for (int year = 2020; year <= 2025; year++) {
         for (int month = 1; month <= 12; month++) {
             if (year == 2025 && month > 4) break;
+            if (year == 2023 && month >= 2 && month <= 5) continue;
                 printf("%04d%02d\n", year, month);
         }
     }
