21e0d827115056403d54898fa6cde8eb1e66d79e
25kopo22 <bulal123456@gmail.com>
2025-04-14 13:54:09 +0900

    c - 학생 정보 입력.c

--- c - 학생 정보 입력.c
+++ c - 학생 정보 입력.c
@@ -57,18 +57,11 @@ int main() {
     struct group people[100];
     struct group *p = people;
     int count = 3;
-
     strcpy(p[0].name, "hong"); p[0].age = 30; p[0].sex = 'M'; strcpy(p[0].hobby, "soccer");
     strcpy(p[1].name, "kim");  p[1].age = 25; p[1].sex = 'F'; strcpy(p[1].hobby, "music");
     strcpy(p[2].name, "park"); p[2].age = 20; p[2].sex = 'M'; strcpy(p[2].hobby, "movie");
-
-    printf("초기 사용자 정보:\n");
-    printMembers(p, count);
-    printf("\n");
-    
-    while (TRUE) {
-        inputOneMember(p, &count);
-    }
-    
+    printf("초기 사용자 정보:\n"); 
+    printMembers(p, count); 
+    while (TRUE) inputOneMember(p, &count);
     return 0;
 }
